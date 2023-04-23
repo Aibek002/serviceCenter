bce52e24368c184112bc5773cb31a40b00ada42f
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-23 21:05:28 +0600

    commit

---
+++ src/components/Content/content.jsx
@@ -0,0 +1,17 @@
+
+import s from './content.module.css';
+
+
+
+function Content() {
+  return (
+    <div className={s.content}>
+
+            <div className={s.item}></div>
+            
+    
+    </div>
+  );
+}
+
+export default Content;
