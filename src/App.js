f9db719c9f9c8a152b1f099ff194ee6edf9e0923
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-24 20:48:08 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Content from './components/Content/content';
 import Header from './components/Header/header';
+import Services from './components/Services/services';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Content/>
+      <Services/>
     </div>
   );
 }
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -7,12 +7,14 @@ function Header() {
   return (
     <div className={s.header}>
 
-            <div className={s.item}><img className={s.imgLogo} src="https://static.tildacdn.com/tild3231-3336-4334-a266-366539343965/2.png" alt="logoGamaGroup" /></div>
-            <div className={s.item}></div>
-            <div className={s.item}><a className={s.a} href="/">Главная</a> <a className={s.a}  href="/">Услуги</a> <a className={s.a}  href="/">Информация</a> </div>
-            <div className={s.item}><img className={s.imgMessage} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png" alt="" /><img className={s.imgMessage} src="https://cdn.icon-icons.com/icons2/2201/PNG/512/telegram_logo_circle_icon_134012.png" alt="" /></div>
-            
-    
+      <div className={s.item}><img className={s.imgLogo} src="https://static.tildacdn.com/tild3231-3336-4334-a266-366539343965/2.png" alt="logoGamaGroup" /></div>
+      <div className={s.item}><a className={s.a} href="/">Главная</a> <a className={s.a} href="/">Услуги</a> <a className={s.a} href="/">Информация</a> </div>
+      <div className={s.item}>
+        <img className={s.imgMessage} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png" alt="" />
+        <img className={s.imgMessage} src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png" alt="" />
+      </div>
+<div className={s.item}><button>+7 777 777 77 77</button></div>
+
     </div>
   );
 }
