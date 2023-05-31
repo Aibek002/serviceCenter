ae61cf8973804c3eddafbbd48988fb1963c67dc4
Aibek002 <200103346@stu.sdu.edu.kz>
2023-05-31 23:42:04 +0600

    modified:   src/components/Services/services.jsx

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -14,21 +14,16 @@ function Services() {
             <div className={s.menu}>
                 <div className={s.item}>
             
-                    <img src="https://cdn-icons-png.flaticon.com/512/2557/2557959.png" alt="" />
                     <p className={s.titleText}>Оценка движимого и недвижимого имущества</p>
                     <p className={s.text}>Владельцам движимого и недвижимого имущества предлагаем...</p>
-                </div>
-                <div className={s.item}>
-                <img src="https://cdn-icons-png.flaticon.com/512/2394/2394338.png" alt="" />
+               
                     <p className={s.titleText}>Оценка интелектульной собственности </p>
                     <p className={s.text}>Оцениваем нематериальные активы, право требования...</p>
-                </div>
-                <div className={s.item}>
+               
                 <img src="https://www.weiss-chemie.com/fileadmin/_processed_/d/f/csm_company-icon_f159fa11f0.png" alt="" />
                     <p className={s.titleText}>Оценка коммерческого недвижимого имущества</p>
                     <p className={s.text}>Оцениваем магазины, заводы, незавершенное строительство, АЗС и...</p>
-                </div>
-                <div className={s.item}>
+         
                 <img src="https://cdn-icons-png.flaticon.com/512/4955/4955649.png" alt="" />
                     <p className={s.titleText}>Оценка ущерба</p>
                     <p className={s.text}>Независимая оценка ущерба поможет вам создать юридическую...</p>
