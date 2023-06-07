7568f750cdacc320692117546872c2fdb69489fa
Aibek002 <200103346@stu.sdu.edu.kz>
2023-06-07 17:35:56 +0600

    modified:   src/components/Content/content.jsx 	modified:   src/components/Content/content.module.css 	modified:   src/components/Header/header.module.css

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -33,19 +33,19 @@ function Content() {
           <div className={s.flex}>
             <div className={s.flexItem1}>
               <div style={logoStyle}> <img src="https://assets.nicepagecdn.com/80badba9/4873990/images/597177-39fea02a.png" className={s.logoFlex} alt="" />
-                <h3>НАШИ КОНТАКТЫ</h3></div>
+                <h3 className={s.h3}>НАШИ КОНТАКТЫ</h3></div>
               <p style={textContactUs}>
                 +7 (727) 327 27 73 ,<br />
                 +7 (707) 327 27 73 ,<br />
                 info@gamagroup.kz
               </p>
               <div style={logoStyle}> <img src="https://assets.nicepagecdn.com/80badba9/4873990/images/3177361-cd9ee2c6.png" className={s.logoFlex} alt="" />
-                <h3>НАШ АДРЕС</h3></div>
+                <h3 className={s.h3}>НАШ АДРЕС</h3></div>
               <p style={textContactUs}>
                 Республика Казахстан, Алматы,<br /> Жамбыла,114/85, офис 133
               </p>
               <div style={logoStyle}> <img src="https://assets.nicepagecdn.com/80badba9/4873990/images/447114-f6e8f45b.png" className={s.logoFlex} alt="" />
-                <h3>ВРЕМЯ РАБОТЫ</h3></div>
+                <h3 className={s.h3}>ВРЕМЯ РАБОТЫ</h3></div>
               <p style={textContactUs}>
                 Пн – Пт …… 10:00 – 20:00,<br />
                 Сб, Вс ..….… Закрыто
