c76edd455724ac86314699f6f382f2858998d5e8
Aibek Seitzhan <90970075+Aibek002@users.noreply.github.com>
2023-06-13 10:54:06 +0600

    Updates

---
+++ precache-manifest.4bcf8e05721b204f40c380e212895c0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d28064b7bfad30a1bcd4",
+    "url": "/static/css/main.db5da50f.chunk.css"
+  },
+  {
+    "revision": "d28064b7bfad30a1bcd4",
+    "url": "/static/js/main.d28064b7.chunk.js"
+  },
+  {
+    "revision": "2511f89111de37f4dae3",
+    "url": "/static/js/1.2511f891.chunk.js"
+  },
+  {
+    "revision": "42439541dc676ca96d1f",
+    "url": "/static/css/2.7b07765a.chunk.css"
+  },
+  {
+    "revision": "42439541dc676ca96d1f",
+    "url": "/static/js/2.42439541.chunk.js"
+  },
+  {
+    "revision": "fd3c736e5b7660449b68",
+    "url": "/static/js/runtime~main.fd3c736e.js"
+  },
+  {
+    "revision": "199470ac74a2837c854d41956eb18a5e",
+    "url": "/static/media/logoHeader.199470ac.png"
+  },
+  {
+    "revision": "c9f624c5be779cd82ea0c2c5c65b0725",
+    "url": "/index.html"
+  }
+];
